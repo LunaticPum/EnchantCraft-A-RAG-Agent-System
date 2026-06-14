6410fd856cce7ed56d8687746149117d8fc42afe
PenguinJP <1637435385@qq.com>
2026-06-14 18:18:43 +0800

    🐛 fix: BaguController缺少java.util.List导入

--- QA-Agent-trigger/src/main/java/cn/pumluda/trigger/controller/BaguController.java
+++ QA-Agent-trigger/src/main/java/cn/pumluda/trigger/controller/BaguController.java
@@ -5,6 +5,7 @@ import cn.pumluda.api.dto.BaguSetResponse;
 import cn.pumluda.api.response.Response;
 import cn.pumluda.domain.bagu.IBaguSkillService;
 import cn.pumluda.types.enums.ResponseCode;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
